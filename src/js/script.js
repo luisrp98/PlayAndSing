8ef96296bc4bfd940d4a4e3f0ad2987e0a7c421c
Luis Romo <luisrp9812@gmail.com>
2023-09-14 11:36:16 -0700

    Ej 3: Added form submit

--- src/js/script.js
+++ src/js/script.js
@@ -16,3 +16,20 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     })
 })
+
+// Agregar un controlador de eventos al formulario
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault() // Prevenir la recarga de la página por defecto
+
+    // Obtener los datos del formulario
+    const nombre = document.getElementById('nombre').value
+    const correo = document.getElementById('correo').value
+    const comentario = document.getElementById('comentario').value
+
+    const message = `Gracias por tu mensaje ${nombre}, nos pondremos en contacto contigo en el correo ${correo} para atender tu mensaje: ${comentario}`
+    alert(message)
+
+    document.getElementById('nombre').value = ''
+    document.getElementById('correo').value = ''
+    document.getElementById('comentario').value = ''
+})
